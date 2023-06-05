b9de7597972aa539aed3dc759fa6476f08f07e4e
Armend Imeri <armend.imeri@kubikos.mk>
2023-06-05 16:56:45 +0200

    updated to syncfusion scheduler

--- AdiSchedule/Appointment.cs
+++ AdiSchedule/Appointment.cs
@@ -4,6 +4,11 @@
     {
         public DateTime Start { get; set; }
         public DateTime End { get; set; }
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int Id { get; set; }
+        public string Subject { get; set; }
         public string Text { get; set; }
     }
 }
